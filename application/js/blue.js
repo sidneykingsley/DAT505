348c366e80e60f16a5ca7453cfefb63e69f9dbd6
Sidney Kingsley <sidney.kingsley@students.plymouth.ac.uk>
2019-01-10 00:01:23 +0000

    new

---
+++ application/js/blue.js
@@ -0,0 +1,146 @@
+var camera, scene, renderer, controls, object;
+var w = window.innerWidth;
+var h = window.innerHeight;
+var footerWidth;
+var i = 0;
+var clickCount = 0;
+var helpCount3 = 0;
+var cloudCount = 0;
+var cloudSwitch = false;
+var clouds = new THREE.Object3D();
+
+window.onload = function() {
+  smoothLoader();
+  soundLoader();
+}
+
+function init() {
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setPixelRatio((window.devicePixelRatio) ? window.devicePixelRatio : 1);
+	renderer.setSize(w, h);
+  renderer.autoClear = true;
+  renderer.setClearColor(0xffffff, 0);
+  document.getElementById('canvas').appendChild(renderer.domElement);
+	scene = new THREE.Scene();
+
+  var mtlLoader = new THREE.MTLLoader();
+  mtlLoader.setPath( 'assets/models/environments/' );
+  var url = "blue.mtl";
+  mtlLoader.load( url, function( materials ) {
+    materials.preload();
+    var objLoader = new THREE.OBJLoader();
+    objLoader.setMaterials( materials );
+    objLoader.setPath( 'assets/models/environments/' );
+    objLoader.load( 'blue.obj', function ( object ) {
+        object.rotation.x = 0.3;
+        object.scale.x = object.scale.y = object.scale.z = 0.25;
+        scene.add( object );
+    });
+  });
+
+  var mtlLoader = new THREE.MTLLoader();
+  mtlLoader.setPath( 'assets/models/movers/' );
+  var url = "clouds.mtl";
+  mtlLoader.load( url, function( materials ) {
+    materials.preload();
+    var objLoader = new THREE.OBJLoader();
+    objLoader.setMaterials( materials );
+    objLoader.setPath( 'assets/models/movers/' );
+    objLoader.load( 'clouds.obj', function ( object ) {
+        object.rotation.x = 0.3;
+        object.scale.x = object.scale.y = object.scale.z = 0.25;
+        clouds.add( object );
+    });
+  });
+
+  scene.add(clouds);
+
+	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+	camera.position.z = 3;
+
+  var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+  scene.add( light );
+
+  var light = new THREE.PointLight( 0xffffff, 1, 100 );
+  light.position.set( 30, 50, 50 );
+  scene.add( light );
+
+  var light = new THREE.PointLight( 0xffffff, 1, 100 );
+  light.position.set( 10, 10, 10 );
+  scene.add( light );
+
+  window.addEventListener('resize', onWindowResize, false);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+  console.log(cloudCount);
+  if (cloudCount > 14) cloudSwitch = true;
+  else if (cloudCount < 0) cloudSwitch = false;
+  if (cloudSwitch === true) cloudCount = cloudCount - 0.001;
+  else if (cloudSwitch === false) cloudCount = cloudCount + 0.001;
+  clouds.position.x = cloudCount;
+  renderer.clear();
+	renderer.render(scene, camera);
+}
+
+function randomSpin(min,max){
+  return Math.random() * (max-min) + min ;
+}
+
+function smoothLoader() {
+  var dots = window.setInterval( function() {
+      var wait = document.getElementById("earlyText");
+      if ( wait.innerHTML.length > 2 ) wait.innerHTML = ".";
+      else wait.innerHTML += ".";
+  }, 500);
+  setTimeout(function () {
+    document.getElementById("earlyText").style.opacity = "0";
+  }, 3000);
+  setTimeout(function () {
+    document.getElementById("early").style.opacity = "0";
+        clearInterval(dots);
+  }, 4000);
+  setTimeout(function () {
+    document.getElementById("early").style.zIndex = "0";
+  }, 7000);
+  setTimeout(function () {
+    init();
+    animate();
+    styler();
+  }, 3000);
+}
+
+function styler() {
+  footerWidth = document.getElementById("footerTag").offsetWidth;
+  document.getElementById("footerTag").style.right = `-${footerWidth-15}px`;
+}
+
+function helpClick() {
+  var checker = oddEvenCheck(helpCount3);
+  if (checker === true) {
+    document.getElementById("helpLink").src = "assets/icons/help2.png";
+    document.getElementById("footerTag").style.right = `10px`;
+  }
+  else {
+    document.getElementById("helpLink").src = "assets/icons/help.png";
+    document.getElementById("footerTag").style.right = `-${footerWidth-15}px`;
+  }
+  helpCount3 = helpCount3 + 1;
+}
+
+function oddEvenCheck(x) {
+  if (x % 2 == 0) return true;
+  else return false;
+}
+
+function soundLoader() {
+  document.getElementById("blueSound").play();
+  console.log("played");
+}
